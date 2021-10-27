e760a1f2db5ccc928bb7de763d1bcfca00923caf
mjserq <mjserquina@yahoo.com>
2021-10-27 10:26:22 +0800

    Login panel design fixed

--- index.php
+++ index.php
@@ -17,12 +17,7 @@ Login
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-
-	
-
-	
-	
-	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="main/css/style.css">
 
 	</head>
 	<body>
